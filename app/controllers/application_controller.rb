fed9f4cff8a1fe0ec66fea3783c310058f0bf6cb
shafiq apandi <atthemiko@gmail.com>
2018-09-23 15:30:31 +0800

    devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    helper_method :authenticate_admin
+  
+  protected
+  
+      def configure_permitted_parameters
+          devise_parameter_sanitizer.permit(:account_update, keys: [:username , :password, :password_confirmation])
+      end
+  
+  private
+  
+    def authenticate_admin
+      if current_user.access_level == "user"
+        return redirect_to root_path, notice: "You are not authorized"
+      end
+    end
+  
+  end
+  
