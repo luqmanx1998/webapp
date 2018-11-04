ca206a8d7789e3da6195bcf0192bf0819418ca06
shafiq apandi <atthemiko@gmail.com>
2018-11-04 15:44:08 +0800

    some changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_10_31_100518) do
-=======
 ActiveRecord::Schema.define(version: 2018_10_31_171653) do
->>>>>>> feature/privatepost
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
